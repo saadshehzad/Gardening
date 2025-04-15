c15f47a1eb4a44aaf9fb83f9c8119d780e9cf019
wifi-naqvi <37249@iqraisb.edu.pk>
2025-04-16 02:50:01 +0500

    Add new line

--- plant/models.py
+++ plant/models.py
@@ -16,6 +16,7 @@ class Category(models.Model):
         return self.name
 
 
+
 class Product(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255, unique=True)
