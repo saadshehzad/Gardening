1c2084d1e9aae389100c5dabe2e813c769f2d8ef
wifi-naqvi <37249@iqraisb.edu.pk>
2025-05-02 20:06:47 +0500

    Access token time Updated

--- myproject/settings.py
+++ myproject/settings.py
@@ -166,7 +166,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=300),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
