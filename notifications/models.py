220e971b71c0a673949ff15c67c6027809861591
wifi-naqvi <37249@iqraisb.edu.pk>
2025-07-29 01:12:19 +0500

    Generic to Custom(Registration, Login, Forget Password, Rest Password etc) . Also implemented  "To generate a post share link"

--- notifications/models.py
+++ notifications/models.py
@@ -1,15 +1,14 @@
 from django.db import models
 
+
 class FCMNotification(models.Model):
-    TYPE = (
-        ("Watering", "Watering"),
-    )
+    TYPE = (("Watering", "Watering"),)
     type = models.CharField(max_length=50, choices=TYPE)
     message = models.TextField()
     sent = models.BooleanField(default=False)
     title = models.CharField(max_length=100, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
     def __str__(self):
         return f"{self.title if self.title else 'No Title'}"
--- users/serializers.py
+++ users/serializers.py
@@ -3,13 +3,16 @@ import uuid
 from allauth.account.utils import send_email_confirmation
 from dj_rest_auth.registration.serializers import RegisterSerializer
 from django.conf import settings
+from django.contrib.auth import authenticate, get_user_model
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 from lawn.models import Lawn, UserLawn
-from users.models import User
 
 from .models import *
+
+User = get_user_model()
 
 
 class RegionSerializer(serializers.ModelSerializer):
@@ -22,8 +25,10 @@ class UserRegionProductSerialzier(serializers.Serializer):
     username = serializers.CharField(max_length=30, required=False)
 
 
-class CustomRegisterSerializer(RegisterSerializer, serializers.ModelSerializer):
-    location = serializers.SerializerMethodField()
+class CustomRegisterSerializer(serializers.ModelSerializer):
+    password1 = serializers.CharField(write_only=True, required=True)
+    password2 = serializers.CharField(write_only=True, required=True)
+    location = serializers.CharField(required=True)
 
     class Meta:
         model = User
@@ -35,8 +40,36 @@ class CustomRegisterSerializer(RegisterSerializer, serializers.ModelSerializer):
             return request.data.get("location")
         return None
 
-    def custom_signup(self, request, user):
-        location = self.get_location("obj")
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("A user with this email already exists.")
+        return value
+
+    def validate_username(self, value):
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError(
+                "A user with this username already exists."
+            )
+        return value
+
+    def validate(self, data):
+        if data["password1"] != data["password2"]:
+            raise serializers.ValidationError({"password2": "Passwords do not match."})
+        validate_password(data["password1"])
+        return data
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data["username"],
+            email=validated_data["email"],
+            password=validated_data["password1"],
+            verified=False,
+        )
+        return user
+
+    def save(self, request):
+        user = self.create(self.validated_data)
+        location = self.get_location(user)
 
         fcm_token = request.data.get("fcm_token")
         if fcm_token:
@@ -46,10 +79,71 @@ class CustomRegisterSerializer(RegisterSerializer, serializers.ModelSerializer):
 
         lawn = Lawn.objects.create(name=f"{user.username}'s Lawn")
         UserLawn.objects.create(user=user, lawn=lawn, location=location)
+        return user
 
-    def validate_email(self, value):
-        if User.objects.filter(email=value).exists():
-            raise serializers.ValidationError("Email is already in use.")
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        request = self.context.get("request")
+        self.user = authenticate(
+            request, username=attrs.get("username"), password=attrs.get("password")
+        )
+
+        if self.user:
+            data["username"] = self.user.username
+            data["uid"] = self.user.id
+            data["email"] = self.user.email
+
+            try:
+                profile = UserProfile.objects.get(user=self.user)
+                data["full_name"] = profile.full_name or ""
+                data["region"] = profile.region or ""
+                if profile.image and hasattr(profile.image, "url"):
+                    data["image"] = (
+                        request.build_absolute_uri(profile.image.url)
+                        if request
+                        else profile.image.url
+                    )
+                else:
+                    data["image"] = None
+                data["bio"] = profile.bio or ""
+                data["share_profile"] = profile.share_profile
+                data["share_garden"] = profile.share_garden
+            except UserProfile.DoesNotExist:
+                data["full_name"] = ""
+                data["region"] = ""
+                data["image"] = None
+                data["bio"] = ""
+                data["share_profile"] = False
+                data["share_garden"] = False
+
+        return data
+
+
+class PasswordChangeSerializer(serializers.Serializer):
+    new_password1 = serializers.CharField(write_only=True)
+    new_password2 = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        if data["new_password1"] != data["new_password2"]:
+            raise serializers.ValidationError("Passwords do not match.")
+        validate_password(data["new_password1"], self.context["request"].user)
+        return data
+
+
+class PasswordResetSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+
+
+class PasswordResetConfirmSerializer(serializers.Serializer):
+    new_password = serializers.CharField(required=True)
+
+    def validate_new_password(self, value):
+        validate_password(value)
         return value
 
 
@@ -75,36 +169,3 @@ class UserProfileSerializer(serializers.ModelSerializer):
                 return request.build_absolute_uri(obj.image.url)
             return obj.image.url
         return None
-
-
-class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
-    def validate(self, attrs):
-        data = super().validate(attrs)
-        request = self.context.get("request")
-        data["username"] = self.user.username
-        data["uid"] = self.user.id
-        data["email"] = self.user.email
-        try:
-            profile = UserProfile.objects.get(user=self.user)
-            data["full_name"] = profile.full_name or ""
-            data["region"] = profile.region or ""
-            if profile.image and hasattr(profile.image, "url"):
-                data["image"] = (
-                    request.build_absolute_uri(profile.image.url)
-                    if request
-                    else profile.image.url
-                )
-            else:
-                data["image"] = None
-            data["bio"] = profile.bio or ""
-            data["share_profile"] = profile.share_profile
-            data["share_garden"] = profile.share_garden
-        except UserProfile.DoesNotExist:
-            data["full_name"] = ""
-            data["region"] = ""
-            data["image"] = None
-            data["bio"] = ""
-            data["share_profile"] = False
-            data["share_garden"] = False
-
-        return data
