ce3f0e1a431dff0644042564437cd97dce4bbf94
wifi-naqvi <37249@iqraisb.edu.pk>
2025-07-30 00:36:23 +0500

    Share post with Post's Id from Share_token UUID

--- posts/urls.py
+++ posts/urls.py
@@ -7,11 +7,7 @@ urlpatterns = [
     path("article/", ArticlesListCreateAPIView.as_view()),
     path("report/", ReportProblemListCreateAPIView.as_view(), name="report-problem"),
     path("share/<uuid:post_id>/", UserPostShareAPIView.as_view(), name="share_post"),
-    path(
-        "redirect/<uuid:share_token>/",
-        RedirectToPostView.as_view(),
-        name="redirect_to_post",
-    ),
+    path("redirect/<uuid:post_id>/", RedirectToPostView.as_view(), name="redirect_to_post"),
     path("like/<str:pk>/", UserPostLikeAPIView.as_view(), name="userpost-like"),
     path(
         "comment/<str:pk>/", UserPostCommentAPIView.as_view(), name="userpost-comment"
