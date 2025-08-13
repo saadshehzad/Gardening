5b5e1a2fbe55899414b7f638f504dd18af6ed8f1
Arsl <arslaniqbal12536@gmail.com>
2025-08-14 00:40:42 +0500

    notifications read all and read by uuid

--- notifications/views.py
+++ notifications/views.py
@@ -1,17 +1,44 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
+from datetime import timedelta
+from django.utils import timezone
 from .models import FCMNotification
 from .serializers import NotificationSerializer
-
+from rest_framework import status
 
 class UserNotificationsAPIView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        notifications = FCMNotification.objects.filter(user=request.user).order_by(
-            "-created_at"
-        )
+        seven_days_ago = timezone.now() - timedelta(days=7)
+        
+        notifications = FCMNotification.objects.filter(
+            user=request.user,
+            created_at__gte=seven_days_ago
+        ).order_by('-created_at')
+        
         serializer = NotificationSerializer(notifications, many=True)
         return Response(serializer.data)
+
+class MarkAllNotificationsReadAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def patch(self, request):
+        FCMNotification.objects.filter(
+            user=request.user, 
+            is_read=False
+        ).update(is_read=True)
+        
+        return Response({"detail": "All notifications marked as read."})
+    
+class MarkNotificationReadAPIView(APIView):  
+    permission_classes = [IsAuthenticated]
+    def patch(self, request, id):
+        try:
+            notification = FCMNotification.objects.get(id=id, user=request.user)
+            notification.is_read = True
+            notification.save()
+            return Response({"detail": "Notification marked as read."}, status=status.HTTP_200_OK)
+        except FCMNotification.DoesNotExist:
+            return Response({"message": "Notification not found."}, status=status.HTTP_404_NOT_FOUND)
